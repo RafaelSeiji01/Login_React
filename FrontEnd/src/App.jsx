09daafee22f37700db41c5582e02514e27702ac4
Rafael <seijiarakaki12@gmail.com>
2026-05-02 19:11:46 -0300

    Craindo a tela login

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,49 @@
-import { useState } from 'react';
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='bg-emerald-200'>
-      <header className='bg-yellow-200 text-3xl'>
-        <h1>Login</h1>
-      </header>
+  
+    <div className='bg-gray-300 h-screen w-full flex items-center justify-center'>
+      
+      <div className='max-w-md w-full overflow-hidden shadow-md md:max-w-2xl bg-blue-300 p-12 md:rounded-2xl'>
+          <div> 
+            <header className="flex flex-col gap-3  md">
+              <h1 className='text-center text-4xl md:text-5xl text-white font-medium'>Seja bem vindo</h1> 
+              <p className="text-1xl md:text-3xl text-center text-white font-medium">Entre com usuario e senha</p>
+
+            </header>
+
+            <div className="p-2 flex flex-col gap-4 ">
+
+              <div className=" bg-white border-2 border-white p-2 focus-within:border-pink-400 transition-colors duration-300 ">
+                <input 
+                type="text"
+                id="username"
+                name="username"
+                placeholder="usuario"
+                className="bg-transparent w-full outline-none"
+                />
+              </div>
+
+              <div className=" bg-white border-2 border-white p-2 focus-within:border-pink-400 transition-colors duration-300 ">
+                <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                placeholder="*****" 
+                className="bg-transparent w-full outline-none"
+                />
+              </div>
+
+              <input 
+              type="submit" 
+              value="Login"
+              className=" mt-4 p-2 bg-pink-400 text-white cursor-pointer rounded-md shadow-md hover:bg-pink-500 transition-colors"
+               />
+
+            </div>
+
+          </div>
+      </div>
+
     </div>
   )
 }
